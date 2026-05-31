6b6aed3b671597eff50cb148ed5daebc34a0c695
Syrmpopoulos Stavros & Athina Latifi <stsirbo@gmail.com>
2026-05-31 21:50:26 +0300

    quote page implementation

--- web_app/views.py
+++ web_app/views.py
@@ -15,5 +15,15 @@ def contact(request):
     context = {"page": "contact"}
     return render(request, "contact.html", context)
 def quote(request):
+    if request.method == "POST":
+        customer_name = request.POST.get('full-name')
+        customer_email = request.POST.get('email')
+        customer_phone = request.POST.get('phone')
+        customer_company = request.POST.get('company')
+        customer_service = request.POST.get('service')
+        customer_message = request.POST.get('message')
+        print(customer_name, customer_email, customer_phone, customer_company, customer_service, customer_message)
+        context = {"page": "quote"}
+        return render(request, "quote.html", context)
     context = {"page": "quote"}
     return render(request, "quote.html", context)
